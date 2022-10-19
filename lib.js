ce695ceb3daedc359236cfdaae81c5639f8e662e
Geon Yuk <89954838+Geon27@users.noreply.github.com>
2022-10-19 22:01:11 +0900

    Add files via upload

---
+++ lib.js
@@ -0,0 +1,6 @@
+function over (obj) {
+	obj.src="/Users/geonyuk/Library/Mobile Documents/com~apple~CloudDocs/HTML Study/apple.png"
+}
+function out(obj) {
+	obj.src="/Users/geonyuk/Library/Mobile Documents/com~apple~CloudDocs/HTML Study/banana.png"
+}
